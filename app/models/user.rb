21c70c4288413ebc0df033fcf223e8a169c129c1
rhaynes3k <rhaynes3k@gmail.com>
2021-04-26 22:20:13 -0500

    Added validation errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :sitters
   has_many :jobs
 
+  validates :name, presence: true
+  validates :email, presence: true
+
   def self.create_from_omniauth(auth)
     User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
       u.username = auth['info']['name']
@@ -11,5 +14,7 @@ class User < ApplicationRecord
       u.password = SecureRandom.hex(16)
     end
   end
+
+
 
 end
